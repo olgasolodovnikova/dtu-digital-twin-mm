090de77f5589545f2ecb45f00845c2e9aca0825a
olgasolodovnikova <osolodovnikova@hotmail.com>
2024-01-07 17:35:22 +0100

    added unit in comments.

--- data/Power_goal.py
+++ data/Power_goal.py
@@ -1,5 +1,5 @@
 # Power goal
-power_goal = [0.2, 0.3, 0.5, 1.0 ,2.0, 3.0, 3.5, 3.7, 4.0, 4.1]
+power_goal = [0.2, 0.3, 0.5, 1.0 ,2.0, 3.0, 3.5, 3.7, 4.0, 4.1] # [MW]
 
 for i in range(len(power_goal)):
-    power_goal[i] = power_goal[i]*1e5
+    power_goal[i] = power_goal[i]*1e5 #to W
